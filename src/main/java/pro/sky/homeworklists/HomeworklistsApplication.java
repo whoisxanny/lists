3e9468a0642b84f3616040cd5fa27a18e04fdda8
whoisxanny <tuffx4nny@gmail.com>
2023-12-17 18:13:01 +0500

    Initials

---
+++ src/main/java/pro/sky/homeworklists/HomeworklistsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworklists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworklistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworklistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homeworklists/HomeworklistsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworklists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworklistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
